2c05f8cf916478d0d8743143152c202cdaae6315
hecomi <info@hecomi.com>
2017-08-14 15:59:21 +0900

    add blob test.

---
+++ Assets/uOSC/Examples/Scripts/OscServerBlobTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace uOSC
+{
+
+[RequireComponent(typeof(OscServer)),
+ RequireComponent(typeof(Renderer))]
+public class OscServerBlobTest : MonoBehaviour
+{
+    Texture2D texture_;
+
+    void Start()
+    {
+        var server = GetComponent<OscServer>();
+        server.onDataReceived.AddListener(OnDataReceived);
+
+        texture_ = new Texture2D(256, 256, TextureFormat.ARGB32, true);
+
+        var renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = texture_;
+    }
+
+    void OnDataReceived(OscMessage message)
+    {
+        if (message.address == "/uOSC/blob")
+        {
+            var byteTexture = message.values[0].AsBlob();
+            ImageConversion.LoadImage(texture_, byteTexture, true);
+        }
+    }
+}
+
+}
