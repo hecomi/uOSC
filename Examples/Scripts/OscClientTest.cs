4ab2b93a38d3cb288e80240902750c5a9b0afe78
hecomi <info@hecomi.com>
2017-08-14 12:17:15 +0900

    add client-to-server test.

--- Examples/Scripts/OscClientTest.cs
+++ Examples/Scripts/OscClientTest.cs
@@ -9,7 +9,7 @@ public class OscClientTest : MonoBehaviour
     void Update()
     {
         var client = GetComponent<OscClient>();
-        client.Send("/uOSC/test", 10, "hoge", "hogehoge", 1.234f, 123);
+        client.Send("/uOSC/test", 10, "hoge", "hogehoge", 1.234f, 123f);
     }
 }
 
