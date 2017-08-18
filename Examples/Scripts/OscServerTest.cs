facbf6f24329a1a156768f8001fb580d0bb38559
hecomi <info@hecomi.com>
2017-08-18 14:11:37 +0900

    add server bundle support.

--- Examples/Scripts/OscServerTest.cs
+++ Examples/Scripts/OscServerTest.cs
@@ -23,6 +23,16 @@ public class OscServerTest : MonoBehaviour
             var val5 = message.values[4].AsFloat();
             Debug.LogFormat("{0}: {1} {2} {3} {4} {5}", message.address, val1, val2, val3, val4, val5);
         }
+        else
+        {
+            var msg = message.address + ": ";
+            msg += "(" + message.timestamp.ToLocalTime() + ") ";
+            foreach (var value in message.values)
+            {
+                msg += value.GetString() + " ";
+            }
+            Debug.Log(msg);
+        }
     }
 }
 
