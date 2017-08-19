7e12234c10b934950a68a03e5bb7537095eb4708
hecomi <info@hecomi.com>
2017-08-19 17:00:48 +0900

    add helper to convert datetime to timestamp.

--- Assets/uOSC/Scripts/Core/Timestamp.cs
+++ Assets/uOSC/Scripts/Core/Timestamp.cs
@@ -5,13 +5,29 @@ namespace uOSC
 
 public struct Timestamp
 {
-    public static readonly Timestamp Immediate = new Timestamp(0x1u);
-
     public UInt64 value;
 
     public Timestamp(UInt64 value)
     {
         this.value = value;
+    }
+
+    public static readonly Timestamp Immediate = new Timestamp(0x1u);
+
+    public static Timestamp Now
+    {
+        get { return Timestamp.CreateFromDateTime(DateTime.UtcNow); }
+    }
+
+    public static Timestamp CreateFromDateTime(DateTime time)
+    {
+        var span = time - new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var sec = span.TotalSeconds;
+        var msec = span.TotalMilliseconds - sec * 1000;
+        var integerPart = (UInt32)sec;
+        var decimalPart = (UInt32)msec;
+        var timestamp = ((UInt64)sec << 32) | (UInt64)msec;
+        return new Timestamp(timestamp);
     }
 
     public DateTime ToUtcTime()
