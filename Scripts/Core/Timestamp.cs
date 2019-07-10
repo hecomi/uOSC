d1deb6edf4055302ca9498ee3aef55095369abaa
hecomi <info@hecomi.com>
2019-07-11 01:35:15 +0900

    fix timestamp bug #1.

--- Scripts/Core/Timestamp.cs
+++ Scripts/Core/Timestamp.cs
@@ -22,21 +22,20 @@ public struct Timestamp
     public static Timestamp CreateFromDateTime(DateTime time)
     {
         var span = time - new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        var sec = span.TotalSeconds;
-        var msec = span.TotalMilliseconds - sec * 1000;
-        var integerPart = (UInt32)sec;
-        var decimalPart = (UInt32)msec;
+        var msec = (UInt64)span.TotalMilliseconds;
+        var integerPart = msec / 1000;
+        var decimalPart = ((msec % 1000) * 0x100000000L) / 1000;
         var timestamp = ((UInt64)integerPart << 32) | (UInt64)decimalPart;
         return new Timestamp(timestamp);
     }
 
     public DateTime ToUtcTime()
     {
-        var integerPart = (UInt32)((value >> 32) & 0xFFFFFFFF); 
-        var decimalPart = (UInt32)(value & 0xFFFFFFFF); 
-        var msec = (UInt32)(((Double)decimalPart / UInt32.MaxValue) * 1000); 
+        var integerPart = (UInt64)((value >> 32) & 0xFFFFFFFF); 
+        var decimalPart = (UInt64)(value & 0xFFFFFFFF);
+        var msec = (integerPart * 1000) + ((decimalPart * 1000) / 0x100000000L);
         var baseDate = new DateTime(1900, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        return baseDate.AddSeconds(integerPart).AddMilliseconds(msec);
+        return baseDate.AddMilliseconds(msec);
     }
 
     public DateTime ToLocalTime()
